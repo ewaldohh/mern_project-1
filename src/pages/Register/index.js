07d36098b6693b8efb41ea8d7bd7a187920fa3d6
ewaldohh <heditianto@yahoo.co.id>
2022-04-18 14:43:13 +0700

    New Menu

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { RegisterBg } from '../../assets';
+import { Input } from '../../components';
 import './register.scss';
 
 const Register = () => {
   return (
     <div className='main-page'>
         <div className='left'>
-            <img src={RegisterBg} className='bg-img'/>
+            <img src={RegisterBg} className='bg-img' alt='imageBg'/>
         </div>
         <div className='right'>
-            <p>Form Registrasi</p>
+            <p className='title'>Form Registrasi</p>
+            <Input label='Full Name' placeholder='Full Name'/>
+            <Input label='Email' placeholder='Email'/>
+            <Input label='Password' placeholder='Password'/>
         </div>
     </div>
   )
